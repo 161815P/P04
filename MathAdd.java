bd250512b11bd5ae03c55330d275d12645d3df4a
161815P <33445265+161815P@users.noreply.github.com>
2017-11-08 11:35:18 +0800

    Add files via upload

---
+++ MathAdd.java
@@ -0,0 +1,11 @@
+public class MathAdd {
+	
+	public MathAdd {}
+	
+	static public int add(int a, int b) {   
+        return a + b; 
+	}
+	
+	System.println("Result: " + MathAdd);
+	
+}
---
+++ MathSub.java
@@ -0,0 +1,11 @@
+public class MathSub {
+	
+	public MathSub {}
+	
+	static public int sub(int a, int b) {   
+        return a - b; 
+	}
+	
+	System.println("Result: " + MathSub);
+	
+}
